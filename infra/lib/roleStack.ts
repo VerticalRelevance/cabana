a32a10b9f06654888b365f0221ad7a82e936a95c
Douglas Naphas <douglasnaphas@gmail.com>
2023-08-09 11:39:59 -0400

    Limit the subject to the current branch
    https://github.com/VerticalRelevance/cabana/issues/7
    
    The last few commits have pertained to the above issue.

--- infra/lib/roleStack.ts
+++ infra/lib/roleStack.ts
@@ -7,11 +7,11 @@ export interface RoleStackProps extends StackProps {
 export class RoleStack extends Stack {
   constructor(scope: App, id: string, props: RoleStackProps) {
     super(scope, id, props);
-    const { providerArn } = props;
+    const { providerArn, subject } = props;
     const gitHubRole = new iam.Role(this, "GitHubRole", {
       assumedBy: new iam.WebIdentityPrincipal(providerArn, {
         StringLike: {
-          ["token.actions.githubusercontent.com:sub"]: "",
+          ["token.actions.githubusercontent.com:sub"]: subject,
         },
         StringEquals: {
           ["token.actions.githubusercontent.com:aud"]: "sts.amazon.com",
