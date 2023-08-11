b8e2429d7546b3428fbea2cb5edcb7340a8c8012
Douglas Naphas <douglasnaphas@gmail.com>
2023-08-11 11:43:52 -0400

    Deploy the mgmt read role automatically
    gh-8

--- infra/deploy-mgmt-read-role-stack.sh
+++ infra/deploy-mgmt-read-role-stack.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 STACKNAME=$(npx @cdk-turnkey/stackname@2.1.0 --suffix mgmtread --hash 6)
 npx cdk bootstrap
-npx cdk deploy ${STACKNAME}
+npx cdk deploy --require-approval never ${STACKNAME}
